bb0c588492e43b798cfa776955bd3d85c61d9d62
Oliver Xu <oxu_e@hotmail.com>
2017-08-06 20:47:56 -0400

    work to make client class refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 function initApp(){
   var keyMan = new Key();
 
-  var socket = io();
-  socket.connect('localhost:2000');
+  var testSocket = io();
+  var cli = new cksync.Client(testSocket, 'localhost:2000');
+
+  var socket = io('localhost:2000');
+  socket.connect();
   var prevNetUp = 0;
 
   const NETUPINTERVAL = 1000;
@@ -21,34 +24,35 @@ function initApp(){
   let randomClockOffset = Math.random() * 2000;
   let timeSyncCompensation = 0;
   let ping = 0;
-  let speed = 400;
+  let speed = 100;
+  let myLocalTimestamp = 0;
 
   socket.on('id', (_id) => {
     console.log('id'+_id);
     myId = _id;
     const key = _id+'';
-    valueStore[key] = new C0(key, 100);
+    valueStore[key] = new cksync.C0(key, 100);
   });
 
   socket.on('pingtest', (data) => {
     if(pingSyncId == data.sid){
-      ping = (myRealTimestamp - data.timestamp) / 2;
+      ping = (myLocalTimestamp - data.timestamp) / 2;
       let actual = data.serverTimestamp - ping;
-      if(pingSyncId == 0){
+      //if(pingSyncId == 0){
         timeSyncCompensation = actual - data.timestamp;
-      }else{
-        timeSyncCompensation += actual - data.timestamp;
-      }
+      //}else{
+        //timeSyncCompensation += actual - data.timestamp;
+      //}
     }
     pingSyncId ++;
   });
 
   socket.on('c0', (msg) => {
-    var receivedC0 = C0.decode(msg);
+    var receivedC0 = cksync.C0.decode(msg);
 
     let c0 = valueStore[receivedC0.key + ""];
     if (!c0) {
-      c0 = new C0(receivedC0.key);//, receivedC0.value, receivedC0.delta, receivedC0.requestSequenceNumber);
+      c0 = new cksync.C0(receivedC0.key, receivedC0.value);// receivedC0.delta, receivedC0.requestSequenceNumber);
       valueStore[receivedC0.key] = c0;
     }
 
@@ -81,15 +85,18 @@ function initApp(){
 
   step = (timestamp) => {
     window.requestAnimationFrame(step.bind(this));
-    myRealTimestamp = timestamp + randomClockOffset + timeSyncCompensation;
+    myLocalTimestamp = timestamp + randomClockOffset;
+    myRealTimestamp = myLocalTimestamp + timeSyncCompensation;
+
+    cli.update(timestamp);
 
     ela = myRealTimestamp - then;
 
     // strict time synch update
-    if(myRealTimestamp - prevNetUp > NETUPINTERVAL){
-      prevNetUp = myRealTimestamp - (ela % 1000/30);
+    if(myLocalTimestamp - prevNetUp > NETUPINTERVAL){
+      prevNetUp = myLocalTimestamp - ((myLocalTimestamp - prevNetUp) % NETUPINTERVAL);
 
-      socket.emit('pingtest', ({timestamp: myRealTimestamp, sid: pingSyncId}));
+      socket.emit('pingtest', ({timestamp: myLocalTimestamp, sid: pingSyncId}));
     }
 
     // buffered network updates
@@ -140,7 +147,7 @@ function initApp(){
         myVar.requestSequenceNumber ++;
         myVar.requestTimestamp = myRealTimestamp;
         console.log(frameNum);
-        socket.emit('c0', C0.encode(myVar));
+        socket.emit('c0', cksync.C0.encode(myVar));
       }
       
       // update
